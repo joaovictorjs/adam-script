c96af9cb5ba9f69165e386a51ee4775a914d4b77
joaovictorjs <skai7ami@gmail.com>
2025-12-27 12:44:50 -0300

    test: create tests for lexing semi, equals, let, const and identifiers

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -237,6 +237,163 @@ func Test_GivenSource_WhenGenerateTokens_ThenShouldReturnCorrectTokenSequence(t 
 				{Kind: EOF, Lexeme: "", Position: 7},
 			},
 		},
+		{
+			name:   "let keyword",
+			source: "let",
+			expectedTokens: []Token{
+				{Kind: Let, Lexeme: "let", Position: 0},
+				{Kind: EOF, Lexeme: "", Position: 3},
+			},
+		},
+		{
+			name:   "const keyword",
+			source: "const",
+			expectedTokens: []Token{
+				{Kind: Const, Lexeme: "const", Position: 0},
+				{Kind: EOF, Lexeme: "", Position: 5},
+			},
+		},
+		{
+			name:   "simple identifier",
+			source: "x",
+			expectedTokens: []Token{
+				{Kind: Identifier, Lexeme: "x", Position: 0},
+				{Kind: EOF, Lexeme: "", Position: 1},
+			},
+		},
+		{
+			name:   "identifier with underscore",
+			source: "my_var",
+			expectedTokens: []Token{
+				{Kind: Identifier, Lexeme: "my_var", Position: 0},
+				{Kind: EOF, Lexeme: "", Position: 6},
+			},
+		},
+		{
+			name:   "identifier with numbers",
+			source: "var123",
+			expectedTokens: []Token{
+				{Kind: Identifier, Lexeme: "var123", Position: 0},
+				{Kind: EOF, Lexeme: "", Position: 6},
+			},
+		},
+		{
+			name:   "equals operator",
+			source: "=",
+			expectedTokens: []Token{
+				{Kind: Equals, Lexeme: "=", Position: 0},
+				{Kind: EOF, Lexeme: "", Position: 1},
+			},
+		},
+		{
+			name:   "semicolon",
+			source: ";",
+			expectedTokens: []Token{
+				{Kind: Semicolon, Lexeme: ";", Position: 0},
+				{Kind: EOF, Lexeme: "", Position: 1},
+			},
+		},
+		{
+			name:   "let declaration",
+			source: "let x = 5;",
+			expectedTokens: []Token{
+				{Kind: Let, Lexeme: "let", Position: 0},
+				{Kind: Identifier, Lexeme: "x", Position: 4},
+				{Kind: Equals, Lexeme: "=", Position: 6},
+				{Kind: NumericLiteral, Lexeme: "5", Position: 8},
+				{Kind: Semicolon, Lexeme: ";", Position: 9},
+				{Kind: EOF, Lexeme: "", Position: 10},
+			},
+		},
+		{
+			name:   "const declaration",
+			source: "const PI = 3;",
+			expectedTokens: []Token{
+				{Kind: Const, Lexeme: "const", Position: 0},
+				{Kind: Identifier, Lexeme: "PI", Position: 6},
+				{Kind: Equals, Lexeme: "=", Position: 9},
+				{Kind: NumericLiteral, Lexeme: "3", Position: 11},
+				{Kind: Semicolon, Lexeme: ";", Position: 12},
+				{Kind: EOF, Lexeme: "", Position: 13},
+			},
+		},
+		{
+			name:   "let with expression",
+			source: "let result = 10 + 20;",
+			expectedTokens: []Token{
+				{Kind: Let, Lexeme: "let", Position: 0},
+				{Kind: Identifier, Lexeme: "result", Position: 4},
+				{Kind: Equals, Lexeme: "=", Position: 11},
+				{Kind: NumericLiteral, Lexeme: "10", Position: 13},
+				{Kind: Plus, Lexeme: "+", Position: 16},
+				{Kind: NumericLiteral, Lexeme: "20", Position: 18},
+				{Kind: Semicolon, Lexeme: ";", Position: 20},
+				{Kind: EOF, Lexeme: "", Position: 21},
+			},
+		},
+		{
+			name:   "const with complex expression",
+			source: "const value = (5 * 3) - 2;",
+			expectedTokens: []Token{
+				{Kind: Const, Lexeme: "const", Position: 0},
+				{Kind: Identifier, Lexeme: "value", Position: 6},
+				{Kind: Equals, Lexeme: "=", Position: 12},
+				{Kind: LParen, Lexeme: "(", Position: 14},
+				{Kind: NumericLiteral, Lexeme: "5", Position: 15},
+				{Kind: Star, Lexeme: "*", Position: 17},
+				{Kind: NumericLiteral, Lexeme: "3", Position: 19},
+				{Kind: RParen, Lexeme: ")", Position: 20},
+				{Kind: Minus, Lexeme: "-", Position: 22},
+				{Kind: NumericLiteral, Lexeme: "2", Position: 24},
+				{Kind: Semicolon, Lexeme: ";", Position: 25},
+				{Kind: EOF, Lexeme: "", Position: 26},
+			},
+		},
+		{
+			name:   "multiple declarations",
+			source: "let x = 1; const y = 2;",
+			expectedTokens: []Token{
+				{Kind: Let, Lexeme: "let", Position: 0},
+				{Kind: Identifier, Lexeme: "x", Position: 4},
+				{Kind: Equals, Lexeme: "=", Position: 6},
+				{Kind: NumericLiteral, Lexeme: "1", Position: 8},
+				{Kind: Semicolon, Lexeme: ";", Position: 9},
+				{Kind: Const, Lexeme: "const", Position: 11},
+				{Kind: Identifier, Lexeme: "y", Position: 17},
+				{Kind: Equals, Lexeme: "=", Position: 19},
+				{Kind: NumericLiteral, Lexeme: "2", Position: 21},
+				{Kind: Semicolon, Lexeme: ";", Position: 22},
+				{Kind: EOF, Lexeme: "", Position: 23},
+			},
+		},
+		{
+			name:   "declaration without spaces",
+			source: "let x=5;",
+			expectedTokens: []Token{
+				{Kind: Let, Lexeme: "let", Position: 0},
+				{Kind: Identifier, Lexeme: "x", Position: 4},
+				{Kind: Equals, Lexeme: "=", Position: 5},
+				{Kind: NumericLiteral, Lexeme: "5", Position: 6},
+				{Kind: Semicolon, Lexeme: ";", Position: 7},
+				{Kind: EOF, Lexeme: "", Position: 8},
+			},
+		},
+		{
+			name:   "identifier that starts with keyword",
+			source: "letter",
+			expectedTokens: []Token{
+				{Kind: Identifier, Lexeme: "letter", Position: 0},
+				{Kind: EOF, Lexeme: "", Position: 6},
+			},
+		},
+		{
+			name:   "identifier similar to const",
+			source: "constant",
+			expectedTokens: []Token{
+				{Kind: Identifier, Lexeme: "constant", Position: 0},
+				{Kind: EOF, Lexeme: "", Position: 8},
+			},
+		},
 	}
 
 	for _, testcase := range testcases {
